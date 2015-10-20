e79bb4259423ff3f07c0c1a88283294e2328f4ac
smurfik <smurfik@mlpinit.com>
2015-10-20 12:07:32 -0700

    add self.all & attributes to Vendor class

---
+++ lib/far_mar/vendor.rb
@@ -0,0 +1,28 @@
+class FarMar::Vendor
+  attr_accessor :id,
+                :name,
+                :no_of_employees,
+                :market_id
+
+  def initialize(attrs)
+    @id              = attrs[:id]
+    @name            = attrs[:name]
+    @no_of_employees = attrs[:no_of_employees]
+    @market_id       = attrs[:market_id]
+  end
+
+  def self.path
+    "/Users/tamarapop/code_builders/projects/far_mar/support/vendors.csv"
+  end
+
+  def self.all
+    CSV.open(path).map do |line|
+      new(
+        id:              line[0].to_i,
+        name:            line[1],
+        no_of_employees: line[2],
+        market_id:       line[3].to_i
+      )
+    end
+  end
+end
