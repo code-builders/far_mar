8cad6870fcf9936feee6426c7eab819e30c11855
smurfik <smurfik@mlpinit.com>
2015-10-19 20:53:02 -0700

    remove variables from do block, create method path

--- lib/far_mar/market.rb
+++ lib/far_mar/market.rb
@@ -1,3 +1,42 @@
 class FarMar::Market
+  attr_accessor :id,
+                :name,
+                :address,
+                :city,
+                :county,
+                :state,
+                :zip
+
+  def initialize(attrs)
+    @id      = attrs[:id]
+    @name    = attrs[:name]
+    @address = attrs[:address]
+    @city    = attrs[:city]
+    @county  = attrs[:county]
+    @state   = attrs[:state]
+    @zip     = attrs[:zip]
+  end
+
+  def self.path
+    "/Users/tamarapop/code_builders/projects/far_mar/support/markets.csv"
+  end
+
+  def self.all
+    CSV.open(path).map do |line|
+      new(
+        id:        line[0],
+        name:      line[1],
+        address:   line[2],
+        city:      line[3],
+        county:    line[4],
+        state:     line[5],
+        zip:       line[6]
+      )
+    end
+  end
+
+  def self.find(id)
+
+  end
 
 end
