ac68509c93847482de62b7dc7b6e1d4a3b62b543
Daniel Adler <weston.adler@gmail.com>
2015-10-20 14:47:24 -0700

    .find and .find_all by_state methods for Market class

--- lib/far_mar/market.rb
+++ lib/far_mar/market.rb
@@ -20,7 +20,7 @@ class FarMar::Market
     @zip        = attrs[:zip]
   end
 
-  # INSTANCE VARIABLES:
+### CLASS VARIABLES:
   @all_markets = CSV.open("/Users/danieladler/Programming/Code_Builders/ruby_practice/projects/far_mar/support/markets.csv", "r")
 
   @attributes = @all_markets.map do |m| # will return array of hashes, each containing all info per market
@@ -34,6 +34,7 @@ class FarMar::Market
     }
   end
 
+### SHARED CLASS METHODS
   # CLASS METHOD 1
   def self.all # returns array of 500 FarMar::Market objects, each of which contains hash of all relevant data
 
@@ -48,9 +49,10 @@ class FarMar::Market
 
   # CLASS METHOD 2:
   def self.find (id)
+
     all_markets = self.all
 
-    all_markets.find {|m| m.id == id.to_s}
+    all_markets.find {|m| m.id.to_i == id.to_i}
 
     # this works:
       # puts "id of random market"
@@ -61,4 +63,22 @@ class FarMar::Market
       # found_id = @all_markets_as_objects.find {|m| m.id == "1"}
       # puts found_id.name
   end
+
+### UNIQUE CLASS METHODS:
+  # vendors - returns a collection of Vendor instances that are associated with the market by the market_id field.
+  def self.vendors
+    #TBD! Set up Vendors class first.
+  end
+
+  def self.find_by_state (state_name)
+    all_markets = self.all
+    all_markets.find {|m| m.state == state_name}
+
+  end
+
+  def self.find_all_by_state (state_name)
+    all_markets = self.all
+    all_markets.find_all {|m| m.state == state_name}
+  end
+
 end
