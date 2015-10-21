2c62ad12f106f62525b05d36423ee14dac393bd0
Daniel Adler <weston.adler@gmail.com>
2015-10-21 15:46:12 -0700

    .products, .sales methods and 100% pass spec for Vendor class

--- lib/far_mar/vendor.rb
+++ lib/far_mar/vendor.rb
@@ -1,9 +1,10 @@
 # TO DO:
+# - get .revenue method working
 # - clarify self.by_market method with BW. What should it return?
 # - DRY: add .read_file class method, refactor .all, .find to use .read_file
 
 # COMMIT NOTES:
-# - 
+# -
 
 require 'csv'
 
@@ -12,27 +13,29 @@ class FarMar::Vendor
   # Each vendor belongs to a market, the market_id field relates to the
   # Market ID field. Each vendor has many products they sell.
 
-  attr_accessor :id, :name, :employees, :market_id
+  attr_accessor :id, :name, :no_of_employees, :market_id
 
   def initialize (attrs)
-    @id         = attrs[:id].to_i
-    @name       = attrs[:name]
-    @employees  = attrs[:employees]
-    @market_id  = attrs[:market_id].to_i
+    @id               = attrs[:id].to_i
+    @name             = attrs[:name]
+    @no_of_employees  = attrs[:no_of_employees].to_i
+    @market_id        = attrs[:market_id].to_i
   end
 
 ### CLASS VARIABLES:
   @all_vendors = CSV.open("/Users/danieladler/Programming/Code_Builders/ruby_practice/projects/far_mar/support/vendors.csv", "r")
 
   @attributes = @all_vendors.map do |v| # will return array of hashes, each containing all info per vendor
-    { id:        v[0],
-      name:      v[1],
-      employees: v[2],
-      market_id: v[3]
+    { id:              v[0],
+      name:            v[1],
+      no_of_employees: v[2],
+      market_id:       v[3]
     }
   end
 
 ### SHARED CLASS METHODS
+  # DRY: Add self.read_file method here
+
   # CLASS METHOD 1
   def self.all # returns array of #2690 FarMar::Vendor objects, each of which contains hash of all CSV data
     all_vendors_as_objects = @attributes.map do |v|
@@ -56,19 +59,27 @@ class FarMar::Vendor
     match = all_markets.find {|m| m.id == @i} # => id of Market object matches mkt_id of Vendor
   end
 
-  # products - returns a collection of Product instances that are associated with vendor by the Product vendor_id field.
-  def products
-    # TBD! Set up Product class first.
+  def products #ret. Product instances assoc w/ vendor by the Product vendor_id field.
+    all_products = FarMar::Product.all
+    match = all_products.find_all {|p| p.vendor_id == self.id}
   end
 
-  # sales - returns a collection of Sale instances that are associated with vendor by the vendor_id field.
-  def sale
-    # TBD! Set up Sale class first
+
+  def sales #ret. Sale instances assoc w/ vendor by vendor_id field.
+    all_sales = FarMar::Sale.all
+    match = all_sales.find_all {|s| s.vendor_id == self.id}
   end
 
-  # revenue - returns the the sum of all of the vendor's sales (in cents)
-  def revenue #don't think this needs .self -- it's an instance method b/c on specific vendor instance
-    # TBD! Set up Sale (or Product?) class first
+  #don't think this needs .self in method name-- it's an instance method b/c on specific vendor instance
+  def revenue #ret sum of all vendor's sales (in cents)
+    # puts "self when this far into .revenue method:"
+    # puts self.class # => FM::Vendor
+    # all_sales = []
+    # sum = []
+    # self.sales.each {|s| all_sales << s.amount} # => here, self is this instance of Vendor
+    # sum = all_sales.each {|a| sum += a}
+    # puts sum
+
   end
 
   # self.by_market(market_id) - returns a list of all Vendor objects with a market id that matches the input
