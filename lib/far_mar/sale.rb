78f0ee4b966bdcee50a645d64f2510eb484ca6c4
LeShoyaStarkey <olstarkey@gmail.com>
2015-10-22 15:13:23 -0700

    Refactored sale.rb file and file will return all rows of csv file as objects and return row where ID field matches argument

--- lib/far_mar/sale.rb
+++ lib/far_mar/sale.rb
@@ -3,62 +3,36 @@ require 'csv'
 
 class FarMar::Sale #capitalize all names of class
 
-  attr_reader
-    :id
-    :saleamount
-    :purchasetime
-    :vendorid
-    :productid
+  attr_reader :id, :saleamount, :purchasetime, :vendorid, :productid
 
     def initialize(attrs)
-      @id= attrs[:id]
-      @saleamount=attrs[:saleamount]
-      @purchasetime=attrs[:purchasetime]
-      @vendorid=attrs[:vendorid]
-      @productid=attrs[:productid]
+      @id= attrs[0].to_i
+      @saleamount=attrs[1]
+      @purchasetime=attrs[2]
+      @vendorid=attrs[3]
+      @productid=attrs[4]
 
     end
 
+# self.all - returns all rows of the CSV file as objects
     def self.all
-      new_sale_array = []
-      a=CSV.read("support/sales.csv")#read the CSV file
-      a.each do |row|
-
-          row_info= {
-                id:row[0].to_i,
-                saleamount:row[1],
-                purchasetime: row[2],
-                vendorid: row[3],
-                productid: row[4]}
-    #
-        new_sale = FarMar::Sale.new(row_info)
-        new_sale_array << new_sale
+      read_file.collect do |row|
+      FarMar::Sale.new(row)
       end
-
+    end
+#
+# self.find(id) - returns the row where the ID field matches the argument
+    def self.find(id)
+      read_file.find do |row|
+      id == row[0].to_i
+      end
     end
 
+#Method that reads the file
+    def self.read_file
+      CSV.read("support/sales.csv")
+    end
 
-
-        def self.find(id)
-          new_sale_object = []
-          a=CSV.read("support/sales.csv")#read the CSV file
-          a.each do |row|
-
-              row_info= {
-                    id:row[0].to_i,
-                    saleamount:row[1],
-                    purchasetime: row[2],
-                    vendorid: row[3],
-                    productid: row[4]}
-
-            if row[0].to_i == id
-              newsale = FarMar::Sale.new(row_info)
-              new_sale_object << newsale
-            end
-          end
-          new_sale_object
-        end
-      # new_sale_array
 
 
 
