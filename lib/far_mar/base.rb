6f9e39a833d2b0fe1122ff3d0bcd7f79c0b464bd
smurfik <smurfik@mlpinit.com>
2015-10-23 11:25:36 -0700

    update find(id) method to stop reading when id is found

--- lib/far_mar/base.rb
+++ lib/far_mar/base.rb
@@ -5,7 +5,9 @@ class FarMar::Base
   end
 
   def self.find(id)
-    all.find { |obj| obj.id == id }
+    array = CSV.read(path)
+    row_match = array.find { |row| id == row[0].to_i }
+    new(row_match)
   end
 
 end
