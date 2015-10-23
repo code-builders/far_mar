6df0c04c827b3fe94ebf6701fc5fb1e5cb1b3324
LeShoyaStarkey <olstarkey@gmail.com>
2015-10-23 11:42:04 -0700

    added 2 methods, 1st method returns an array of sale object by vendor id and the 2nd method returns an array of sale objects with a product_id matching the argument

--- lib/far_mar/sale.rb
+++ lib/far_mar/sale.rb
@@ -9,8 +9,8 @@ class FarMar::Sale #capitalize all names of class
       @id= attrs[0].to_i
       @saleamount=attrs[1]
       @purchasetime=attrs[2]
-      @vendorid=attrs[3]
-      @productid=attrs[4]
+      @vendorid=attrs[3].to_i
+      @productid=attrs[4].to_i
 
     end
 
@@ -33,6 +33,20 @@ class FarMar::Sale #capitalize all names of class
       CSV.read("support/sales.csv")
     end
 
+# self.by_vendor(vendor_id) - returns an Array of Sale objects with a vendor_id matching the argument.
+  def self.by_vendor(vendor_id)
+    all.find_all do |x|
+    vendor_id.to_i==x.vendorid
+    end
+  end
+
+# self.by_product(product_id)` - returns an `Array` of `Sale` objects with a `product_id` matching the argument.
+    def self.by_product(product_id)
+      all.find_all do |x|
+      product_id.to_i==x.productid
+    end
+
+    end
 
 
 
