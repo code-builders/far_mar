8a7bc74e5b9eb7618aca96159d887ac60c663848
Karen Hambro <khambro@gmail.com>
2015-10-22 18:56:12 -0700

    adds time function

--- lib/far_mar/sale.rb
+++ lib/far_mar/sale.rb
@@ -1,12 +1,13 @@
 require 'csv'
 
 class FarMar::Sale
-  attr_accessor :id, :amount, :time_purchased, :vendor_id, :product_id
+  attr_accessor :id, :amount, :purchase_time, :vendor_id, :product_id
 
   def initialize(attrs)
+    require 'time'
     @id             = attrs[0].to_i
     @amount         = attrs[1].to_i
-    @time_purchased = attrs[2]
+    @purchase_time  = Time.parse(attrs[2])
     @vendor_id      = attrs[3].to_i
     @product_id     = attrs[4].to_i
   end
@@ -47,13 +48,30 @@ class FarMar::Sale
     all_sale[product_id]
   end
 
-
   def self.by_vendor(vendor_id)
     all_sale = self.all.group_by do |obj|
       obj.vendor_id
     end
     all_sale[vendor_id]
   end
+
+  def self.between(beginning_time, end_time)
+    require 'time'
+    within = []
+    sales_within_block = self.all.group_by do |obj|
+      obj.purchase_time
+      if Time.parse(obj.purchase_time).between?(Time.parse(beginning_time), Time.parse(end_time))
+        within << obj
+      end
+    end
+    within
+  end
+
+
+
+
+
+#  - returns a collection of Sale objects where the purchase time is between the two times given as arguments
 
 
 #self.between(beginning_time, end_time) - returns a collection of Sale objects where the purchase time is between the two times given as arguments
