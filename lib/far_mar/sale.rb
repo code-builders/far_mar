8045fc205ab061f47bcda99a7d107fc72935de47
LeShoyaStarkey <olstarkey@gmail.com>
2015-10-25 12:54:16 -0700

    Added method to return product instance that is associated with sale using sale product_id field

--- lib/far_mar/sale.rb
+++ lib/far_mar/sale.rb
@@ -23,9 +23,10 @@ class FarMar::Sale #capitalize all names of class
 #
 # self.find(id) - returns the row where the ID field matches the argument
     def self.find(id)
-      read_file.find do |row|
+      row_match=read_file.find do |row|
       id == row[0].to_i
       end
+      FarMar::Sale.new(row_match)
     end
 
 #Method that reads the file
@@ -33,23 +34,35 @@ class FarMar::Sale #capitalize all names of class
       CSV.read("support/sales.csv")
     end
 
-# self.by_vendor(vendor_id) - returns an Array of Sale objects with a vendor_id matching the argument.
-  def self.by_vendor(vendor_id)
-    all.find_all do |x|
-    vendor_id.to_i==x.vendorid
+# vendor - returns the Vendor instance that is associated with this sale using the Sale vendor_id field
+    def vendor(sale_vendor_id)
+      FarMar::Vendor.by_vendor(sale_vendor_id)
     end
-  end
+
+# product - returns the Product instance that is associated with this sale using the Sale product_id field
+    def self.product(productid)
+      FarMar::Product.find(productid)
+    end
+
+    # self.between(beginning_time, end_time) - returns a collection of Sale objects where the purchase time is between the two times given as arguments
+    def self.between
+      # read_file.find do |x|
+      # x=
+    end
+
+# self.by_vendor(vendor_id) - returns an Array of Sale objects with a vendor_id matching the argument.
+    def self.by_vendor(vendor_id)
+      all.find_all do |x|
+      vendor_id.to_i==x.vendorid
+      end
+    end
 
 # self.by_product(product_id)` - returns an `Array` of `Sale` objects with a `product_id` matching the argument.
     def self.by_product(product_id)
       all.find_all do |x|
       product_id.to_i==x.productid
+      end
     end
-
-    end
-
-
-
 
 
 
